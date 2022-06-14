64e17551fdddd6b5b6c191f3b9ddbca1cdf60028
ChairWormTOP <chairworm@hotmail.com>
2022-06-14 05:10:18 -0400

    Add function to randomly output rock, paper, or scissors

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const getRandomIntInclusive = (min, max) => {
 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+const computerPlay = () => {
+    //for debudding purposes
+    const randomNumber = getRandomIntInclusive(1, 3)
+    console.log(randomNumber);
+    return options[randomNumber - 1] // randomNumber - 1 because index 0 is 1, index 1 is 2, and index 2 is 3
+}
+
+console.log(computerPlay());
