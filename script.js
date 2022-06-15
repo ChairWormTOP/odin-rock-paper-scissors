27f93a6e18e3ce97ad3aee86e45ede5a1575c9d7
ChairWormTOP <chairworm@hotmail.com>
2022-06-14 21:34:45 -0400

    Add function to play one round of "Rock Paper Scissors"

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const options = ['Rock', 'Paper', 'Scissors']
+const options = ['rock', 'paper', 'scissors']
 
 // helper function to randomly get a value from min to max
 const getRandomIntInclusive = (min, max) => {
@@ -15,4 +15,26 @@ const computerPlay = () => {
     return options[randomNumber - 1] // randomNumber - 1 because index 0 is 1, index 1 is 2, and index 2 is 3
 }
 
-console.log(computerPlay());
+const playSingleRound = (playerSelection, computerSelection) => {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return 'Tie!'
+    } else if ((playerSelection.toLowerCase() === 'rock') && (computerSelection === 'paper')) {
+        return 'You lose! Paper beats Rock'
+    } else if ((playerSelection.toLowerCase() === 'paper') && (computerSelection === 'rock')) {
+        return 'You win! Paper beats Rock'
+    } else if ((playerSelection.toLowerCase() === 'scissors') && (computerSelection === 'paper')) {
+        return 'You win! Scissors beat Paper'
+    } else if ((playerSelection.toLowerCase() === 'paper') && (computerSelection === 'scissors')) {
+        return 'You lose! Scissors beat Paper'
+    } else if ((playerSelection.toLowerCase() === 'rock') && (computerSelection === 'scissors')) {
+        return 'You win! Rock beats Scissors'
+    } else if ((playerSelection.toLowerCase() === 'scissors') && (computerSelection === 'rock')) {
+        return 'You lose! Rock beats Scissors'
+    } else {
+        return 'Invalid input.'
+    }
+}
+
+const playerSelection = prompt("Rock, Paper, or Scissors?")
+
+console.log(playSingleRound(playerSelection, computerPlay()));
